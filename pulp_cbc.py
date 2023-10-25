523bc207b5c61d0d464630def91ee6ff10061370
HietanResearch <hietan.research@gmail.com>
2023-10-25 11:15:07 +0900

    add pulp files

---
+++ pulp_cbc.py
@@ -0,0 +1,21 @@
+import pulp
+
+prob = pulp.LpProblem('PuLP_with_CPLEX', sense = pulp.LpMaximize)
+
+x1 = pulp.LpVariable('x1', cat = 'Continuous')
+x2 = pulp.LpVariable('x2', cat = 'Continuous')
+
+prob += 2 * x1 + 3 * x2
+
+prob += x1 + 3 * x2 <= 9, 'Const1'
+prob += x1 + x2 <= 4, 'Const2'
+prob += 2 * x1 + x2 <= 6, 'Const3'
+
+print(prob)
+
+status = prob.solve()
+
+print()
+print('Status :', pulp.LpStatus[status])
+print('Objective Value :', pulp.value(prob.objective))
+print('(x1, x2) = (' + str(pulp.value(x1)) + ', ' + str(pulp.value(x2)) + ')')
